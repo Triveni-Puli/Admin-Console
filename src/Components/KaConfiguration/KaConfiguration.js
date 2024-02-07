162fe4c211e951773f2e169cde70c39d2070dbd9
Sonika Bansal <sonika.bansal@hcl.com>
2024-02-07 10:21:42 +0530

    Knowledge agent view API integration

--- src/Components/KaConfiguration/KaConfiguration.js
+++ src/Components/KaConfiguration/KaConfiguration.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "../Layout/Dashboard/dashboardStyles.css";
 import Navbar from "../Layout/Dashboard/Navbar";
 import Header from "../Layout/Dashboard/Header";
@@ -8,6 +9,24 @@ import editImg from "../../assets/edit.svg";
 import deleteImg from "../../assets/delete.svg";
 
 const KaConfiguration = () => {
+  const [KACollections, setKACollections] = useState([]);
+    
+
+  useEffect(()=>{
+    axios.get("https://lohbeuf4mgodcuhxj3q343z7o40brjhx.lambda-url.ap-south-1.on.aws/",{
+    },{
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(response => {
+      KACollections.push(response.data)
+      setKACollections(response.data);
+      console.log("response", response.data);
+    }).catch(err => {
+    });
+  },[])
+
+  
   return (
     <>
       <div className="row main">
@@ -37,17 +56,23 @@ const KaConfiguration = () => {
                       <div class="gridThirdCol">Description</div>
                       <div class="gridFourCol">Actions</div>
                   </div>
-                  <div class="gridRow">
-                      <div class="gridFirstCol"><input type="checkbox"></input></div>
-                      <div class="gridSecCol">Global Procurement Services</div>
-                      <div class="gridThirdCol">Information regarding LMS</div>
-                      <div class="gridFourCol">
-                        <img src={showImg} />
-                        <img src={editImg} />
-                        <img src={deleteImg} />
-
-                      </div>
-                  </div>
+                    {
+                      KACollections.map((row, index)=>{
+                        console.log("row", row);
+                        return <>
+                          <div class="gridRow">
+                            <div class="gridFirstCol"><input type="checkbox"></input></div>
+                            <div class="gridSecCol">{row.collection_name}</div>
+                            <div class="gridThirdCol">{row.description}</div>
+                            <div class="gridFourCol">
+                              <img src={showImg} />
+                              <img src={editImg} />
+                              <img src={deleteImg} />
+                            </div>
+                          </div>
+                        </>
+                      })
+                    }
                 </div>
             </div>
 
